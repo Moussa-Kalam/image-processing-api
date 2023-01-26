e01ac06fcc0c4b8a3955613aadc478eef2262e95
Moussa-Kalam <m.amzat@alustudent.com>
2023-01-26 14:45:01 +0200

    Prettier run

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,8 @@ const app = express();
 const port = 8000;
 app.use(logger);
 app.use('/api/images', imagesRoutes);
-// Start the Express server
 
-// You need to check if the folder you store your resized images exists and create it if it does not exist.
+// Start the Express server
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -4,17 +4,18 @@ import supertest from 'supertest';
 const request = supertest(app);
 
 it('should return 200 for GET /api/images', async () => {
-    const res = await request.get('/api/images');
-    expect(res.status).toBe(200);
-  }); 
+  const res = await request.get('/api/images');
+  expect(res.status).toBe(200);
+});
 
 it('should return status 404 for GET /api/image', async () => {
-  const res = await request.get('/api/image');  
+  const res = await request.get('/api/image');
   expect(res.status).toBe(404);
 });
 
 it('should resize image if url is complete', async () => {
-  const res = await request.get('/api/images?filename=santamonica.jpg&width=200&height=200');  
+  const res = await request.get(
+    '/api/images?filename=santamonica.jpg&width=200&height=200'
+  );
   expect(res.status).toBe(200);
 });
-
